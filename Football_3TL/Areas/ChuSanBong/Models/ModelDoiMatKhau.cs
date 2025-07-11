38feeee5486ab66e6106ea266c3512f62bee5b99
BaoAnh79 <nbanhcntt2211026@student.ctuet.edu.vn>
2025-07-11 19:59:12 +0700

    xong đổi mật khẩu

---
+++ Football_3TL/Areas/ChuSanBong/Models/ModelDoiMatKhau.cs
@@ -0,0 +1,9 @@
+﻿namespace Football_3TL.Areas.ChuSanBong.Models
+{
+    public class ModelDoiMatKhau
+    {
+        public String? MatKhauHienTai {  get; set; }
+        public String? MatKhauMoi { get; set; }
+        public String? MatKhauMoi1 { get; set; }
+    }
+}
