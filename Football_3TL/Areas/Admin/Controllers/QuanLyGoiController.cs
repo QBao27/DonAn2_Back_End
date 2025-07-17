0ed4c928ee692f3d76d2925985fdc1b61f04b123
BaoAnh79 <nbanhcntt2211026@student.ctuet.edu.vn>
2025-07-17 15:58:22 +0700

    .

---
+++ Football_3TL/Areas/Admin/Controllers/QuanLyGoiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Football_3TL.Areas.Admin.Controllers
+{
+    public class QuanLyGoiController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
